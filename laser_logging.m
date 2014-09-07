6ba2bdf9ac7bc128f48332f55081bf27bb79b055
Jules-L <jules.laffitte.mobil@gmail.com>
2014-09-06 21:52:39 -0400

    Added laser data logging ability
    The file laser_logging.m runs the sim while an event listener logs the
    lidar data to a csv file using logLaserData.m

---
+++ laser_logging.m
@@ -0,0 +1,44 @@
+rob = neato('sim');
+disp('Simulator launched.');
+
+    % Standard Wall+Obstacle
+    walls = lineObject();
+                  % x  y
+    walls.lines = [-1 -1;
+                    2 -1;
+                    2  2; 
+                   -1  2; 
+                   -1 -1];
+    
+    ob1 = lineObject();
+    ob1.lines = [0.8 0.8; 1 0.8; 1 1; 0.8 1; 0.8 0.8];
+    
+    map = lineMap([walls ob1]);
+    rob.genMap(map.objects)
+    disp('Populated robot''s map.');
+    
+rob.startLaser();
+pause(0.2);
+disp('Robot''s laser is activated.');
+
+global logIndex laserLoggingListener;
+logIndex = 0;
+
+delete('laserData.csv');
+laserLoggingListener = event.listener(rob.laser,'OnMessageReceived',@logLaserData);
+
+rob.sendVelocity(0.1,0.2);
+pause(2);
+rob.sendVelocity(0.3,0.2);
+pause(2);
+rob.sendVelocity(0.3,0.2);
+pause(2);
+rob.sendVelocity(0.3,0.2);
+pause(2);
+rob.sendVelocity(0.1,0.2);
+pause(4);
+
+rob.shutdown();
+disp('Simulator closed.');
+close all;
+clear all;
