a60484132ce728c286278fe2d7bef556f5419d0d
Dan <arnett.daniel@gmail.com>
2014-09-18 20:15:39 -0400

    Added the encoder listeners. This is the non-functioning Lab used for the Lab3 demonstration.

--- lab3.m
+++ lab3.m
@@ -14,6 +14,42 @@ cleanedLaserRanges = rangeDataFilter(rob.neatoRobot.laser.data.ranges);
 % Detect nearest object within a specified sector
 [i,r] = nearestObject(inSector(cleanedLaserRanges, -45, 45));
 
+posePlot = plot(rob.xPositions, rob.yPositions, 'b-');
+set(posePlot, 'Tag', 'posePlot');
+xlim([0.0 0.5]);
+ylim([0.0 0.5]);
+             
+k = 0.314;
+index = 1;
+tic
+while toc < 9
+    if index >= 2
+        rob.theta = rob.theta + rob.omega * rob.dt;
+        rob.xPositions(index) = rob.xPositions(index - 1) + rob.v * cos(rob.theta) * rob.dt;
+        rob.yPositions(index) = rob.yPositions(index - 1) + rob.v * sin(rob.theta) * rob.dt;
+        vr = 0.2+(1/10)*sin(toc*k);
+        vl = 0.2-(1/10)*sin(toc*k);
+    else
+        vr = 0;
+        vl = 0;
+    end
+    set(posePlot, 'xdata', rob.xPositions, ...
+        'ydata', rob.yPositions);
+    
+    index = index + 1;
+
+    rob.neatoRobot.sendVelocity(vr,vl);
+end
+% while toc < 6.5
+%     rob.neatoRobot.sendVelocity(0.2,0.1)
+% end
+% 
+% while toc < 15.3
+%     rob.neatoRobot.sendVelocity(0.1,0.2)
+% end
+pause(5);
+
+if 0
 disp('Launching Error Plot.');
 % Set up error plot
 global error;
@@ -31,7 +67,7 @@ pause(3);
 v = 0.15;
 differential = 0.08;
 
-while true
+while rob.robotActive
     
      plotDistanceError(r,0.65); % plot new error distance
      
@@ -61,6 +97,6 @@ while true
     
     pause(0.05);
 end
-
+end
 %%
 rob.close
