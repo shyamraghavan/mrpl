c58c12e001f2a336e561b7844944b6c2f05d3ca8
Dan <arnett.daniel@gmail.com>
2014-09-18 20:35:52 -0400

    Integrating the Lab3 Robot, encoderListener, and populateSimMap.

---
+++ populateSimMap.m
@@ -0,0 +1,21 @@
+function populateSimMap(obj,n)
+% Loads map number n into the simulator.
+n;
+walls = lineObject();
+
+              % x  y
+walls.lines = [-1 -1;
+                2 -1;
+                2  2; 
+               -1  2; 
+               -1 -1];
+
+ob1 = lineObject();
+ob1.lines = [0.8 0.8; 1 0.8; 1 1; 0.8 1; 0.8 0.8];
+
+map = lineMap([walls ob1]);
+obj.neatoRobot.genMap(map.objects)
+disp('Populated robot''s map.');
+    
+end
+
